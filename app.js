f9f041753c00f9438bb1c1ebbc6215217ef7f86b
Sultan <sultan.singh.atwal@gmail.com>
2020-08-06 17:52:34 +0530

    Added refresh button to start new game

--- app.js
+++ app.js
@@ -1,72 +1,91 @@
 // Selecting elements and declaring global variables
 const grid = document.querySelector('.grid');
 const flagsLeft = document.querySelector('.flags_left');
-const result = document.querySelector('.result')
+const result = document.querySelector('.result');
+const refresh = document.querySelector('#refresh');
+let width;
+let numberBombs;
+let cells;
+let flags;
+let isGameOver;
 
-let width = 10;
-let numberBombs = 10;
-let cells = [];
-let flags = 0;
-let isGameOver = false;
+refresh.addEventListener('click', newGame);
 
-flagsLeft.innerHTML = numberBombs;
+// Start new game
+function newGame() {
+    // Getting rid of the old grid
+    grid.innerHTML = '';
+    result.innerHTML = '';
 
-// Creating shuffled array with bombs at random positions
-const bombArray = Array(numberBombs).fill('bomb');
-const safeArray = Array(width*width - numberBombs).fill('safe');
-const combArray = bombArray.concat(safeArray);
-const shuffledArray = combArray.sort(() => Math.random() - 0.5);
+    // (Re)Setting all variables
+    width = 10;
+    numberBombs = 10;
+    cells = [];
+    flags = 0;
+    isGameOver = false;
 
-for(let i=0; i<width*width; i++) {
-    // Creating cells
-    const cell = document.createElement('div');
-    cell.setAttribute('id', i);
-    cell.classList.add(shuffledArray[i])
-    grid.appendChild(cell);
-    cells.push(cell);
+    // (Re)Setting flags left
+    flagsLeft.innerHTML = numberBombs;
 
-    // Adding event listeners
-    // Normal click
-    cell.addEventListener('click', function(e) {
-        click(cell);
-    });
+    // Creating shuffled array with bombs at random positions
+    const bombArray = Array(numberBombs).fill('bomb');
+    const safeArray = Array(width*width - numberBombs).fill('safe');
+    const combArray = bombArray.concat(safeArray);
+    const shuffledArray = combArray.sort(() => Math.random() - 0.5);
 
-    // Right click
-    cell.oncontextmenu = function(e) {
-        e.preventDefault();
-        addFlag(cell);
+    for(let i=0; i<width*width; i++) {
+        // Creating cells
+        const cell = document.createElement('div');
+        cell.setAttribute('id', i);
+        cell.classList.add(shuffledArray[i])
+        grid.appendChild(cell);
+        cells.push(cell);
+
+        // Adding event listeners
+        // Normal click
+        cell.addEventListener('click', function(e) {
+            click(cell);
+        });
+
+        // Right click
+        cell.oncontextmenu = function(e) {
+            e.preventDefault();
+            addFlag(cell);
+        }
+    }
+
+
+    // Adding numbers to each cell
+    for(let i=0; i<cells.length; i++) {
+        let total = 0;
+        const isLeftEdge = (i % width === 0);
+        const isRightEdge = (i % width === width - 1);
+
+        // Check number of bombs around a cell
+        if(cells[i].classList.contains('safe')) {
+            // West
+            if(i>0 && !isLeftEdge && cells[i-1].classList.contains('bomb')) total++;
+            // North-East
+            if(i>9 && !isRightEdge && cells[i+1-width].classList.contains('bomb')) total++;
+            // North
+            if(i>10 && cells[i-width].classList.contains('bomb')) total++;
+            //North-West
+            if(i>11 && !isLeftEdge && cells[i-1-width].classList.contains('bomb')) total++;
+            // East
+            if(i<98 && !isRightEdge && cells[i+1].classList.contains('bomb')) total++;
+            // South-West
+            if(i<90 && !isLeftEdge && cells[i-1+width].classList.contains('bomb')) total++;
+            // South-East
+            if(i<88 && !isRightEdge && cells[i+1+width].classList.contains('bomb')) total++;
+            //South
+            if(i<89 && cells[i+width].classList.contains('bomb')) total++;
+
+            cells[i].setAttribute('data', total);
+        }
     }
 }
 
-
-// Adding numbers to each cell
-for(let i=0; i<cells.length; i++) {
-    let total = 0;
-    const isLeftEdge = (i % width === 0);
-    const isRightEdge = (i % width === width - 1);
-
-    // Check number of bombs around a cell
-    if(cells[i].classList.contains('safe')) {
-        // West
-        if(i>0 && !isLeftEdge && cells[i-1].classList.contains('bomb')) total++;
-        // North-East
-        if(i>9 && !isRightEdge && cells[i+1-width].classList.contains('bomb')) total++;
-        // North
-        if(i>10 && cells[i-width].classList.contains('bomb')) total++;
-        //North-West
-        if(i>11 && !isLeftEdge && cells[i-1-width].classList.contains('bomb')) total++;
-        // East
-        if(i<98 && !isRightEdge && cells[i+1].classList.contains('bomb')) total++;
-        // South-West
-        if(i<90 && !isLeftEdge && cells[i-1+width].classList.contains('bomb')) total++;
-        // South-East
-        if(i<88 && !isRightEdge && cells[i+1+width].classList.contains('bomb')) total++;
-        //South
-        if(i<89 && cells[i+width].classList.contains('bomb')) total++;
-
-        cells[i].setAttribute('data', total);
-    }
-}
+newGame();
 
 // Add flag with right click
 function addFlag(cell) {
